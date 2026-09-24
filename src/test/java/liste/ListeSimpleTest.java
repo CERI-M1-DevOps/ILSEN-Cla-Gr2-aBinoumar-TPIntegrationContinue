ff1861649b94b746369e03b44a60ad932a266fab
AmineBin <aminebin2004@gmail.com>
2026-09-24 10:35:52 +0200

    ajout : test unitaire

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -256,4 +256,13 @@ class ListeSimpleTest {
         System.out.println(listeATester);
         assertEquals("ListeSimple(Noeud(4), Noeud(2), Noeud(3), Noeud(1), Noeud(5))", listeATester.toString());
     }
+
+    @Test
+    void echangerNoeudEgauxNeFaitRien() {
+        listeATester.ajout(5);
+        Noeud r1 = listeATester.tete; 
+        Noeud r2 = listeATester.tete;
+        ListeATester.echanger(r1, r2);
+        assertEquals("ListeSimple(Noeud(5))", ListeATester.ToString());
+    }
 }
